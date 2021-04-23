6a86f05e8b7ad489a7aa3c5d40ef6f10f651225f
Chase Anderson <anderson.e.chase@gmail.com>
2021-04-23 11:15:49 -0700

    Compolete Step 3.1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Item
 
 # Create your views here.
+
+def index(request):
+  items = Item.objects.all()
+  return render(request, 'index.html', {'items': items})
